7d31641b787b0f2b6419e82b74c0d3aaad4e5cb6
platform1117 <230233@naver.com>
2021-12-31 09:43:37 +0900

    너구리 구구
    66%에서 틀림

---
+++ Baekjoon(ALL)/src/unSolved_Problem/BOJ_18126.java
@@ -0,0 +1,58 @@
+package unSolved_Problem;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+class node {
+	int v;
+	int len;
+
+	node(int v, int len) {
+		this.v = v;
+		this.len = len;
+	}
+}
+
+public class BOJ_18126 {
+	static long ans = 0;
+	static ArrayList<node>[] list;
+	static boolean[] visit;
+
+	static void DFS(int start, long sum) {
+		if (visit[start])
+			return;
+		ans = ans < sum ? sum : ans;
+		visit[start] = true;
+		for (int i = 0; i < list[start].size(); i++) {
+			DFS(list[start].get(i).v, sum + list[start].get(i).len);
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		list = new ArrayList[N + 1];
+		visit = new boolean[N + 1];
+		for (int i = 0; i < N + 1; i++)
+			list[i] = new ArrayList<>();
+		for (int i = 1; i < N; i++) {
+			String[] input = br.readLine().split(" ");
+			int A = Integer.parseInt(input[0]);
+			int B = Integer.parseInt(input[1]);
+			int len = Integer.parseInt(input[2]);
+			list[A].add(new node(B, len));
+			list[B].add(new node(A, len));
+		}
+		DFS(1, 0);
+		bw.write(ans + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
