6c9f3d4d0a677c9abeff69e0f0e8aafe27d14e00
yujongyeop <230233@naver.com>
2022-01-07 14:01:03 +0900

    그대로 출력하기
    단순 구현

---
+++ Baekjoon(ALL)/src/Solved_Problem/BOJ_11718.java
@@ -0,0 +1,17 @@
+package Solved_Problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11718 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		while ((str =br.readLine()) !=null) {
+			System.out.println(str);
+		}
+	}
+
+}
